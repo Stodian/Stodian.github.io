acb7ef36e5ee5c087c15320ef51bc695ea68d096
Ethan Reid <163367994+Stodian@users.noreply.github.com>
2024-04-18 00:51:22 +0000

    map complete

--- Static/Javascript/Map/map_setup.js
+++ Static/Javascript/Map/map_setup.js
@@ -1,12 +1,8 @@
 let map;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
 }
-
-initMap();
